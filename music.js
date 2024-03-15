093c9f8ad751a9e63b24350b5f3b21f94114bd92
SeCrEaT999 <115356905+SeCrEaT999@users.noreply.github.com>
2024-03-15 21:48:04 +0900

    Music!

--- music.js
+++ music.js
@@ -1,13 +1,32 @@
 const cdPlayer = document.getElementById('cd-player');
 const startButton = document.getElementById('start-button');
 const music = document.getElementById('music');
+const cdMusicElement = document.querySelector('.header');
 
-// Function to start music and CD animation
 function startMusic() {
-    music.play();
-    cdPlayer.style.animationPlayState = 'running';
-    startButton.disabled = true; // Disable the start button once music starts
+  music.play();
+// CD 플레이어 요소에 애니메이션 효과를 추가하여 CD를 회전시킵니다.
+  startButton.disabled = true;
 }
 
-// Event listener for the start button
-startButton.addEventListener('click', startMusic);
+window.addEventListener('scroll', function() {
+  const rect = cdMusicElement.getBoundingClientRect();
+  const isInViewport = (rect.top >= 0 && rect.bottom <= window.innerHeight);
+  if (isInViewport) {
+    cdPlayer.style.animationPlayState = 'running';
+    startMusic();
+  }
+});
+
+window.addEventListener('wheel', function(event) {
+  const isScrollingDown = event.deltaY > 0;
+
+  if (isScrollingDown) {
+    const rect = cdMusicElement.getBoundingClientRect();
+    const isInViewport = (rect.top >= 0 && rect.bottom <= window.innerHeight);
+    if (isInViewport) {
+        cdPlayer.style.animationPlayState = 'running';
+        startMusic();
+    }
+  }
+});
